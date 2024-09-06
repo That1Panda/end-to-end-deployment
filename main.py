3b55d4f5841e17c16760f58475a6eff014af8b40
That1Panda <pandahd75@gmail.com>
2024-09-06 19:34:14 +0300

    [Development] Add the model in main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+# Load the tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
+model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
+# Create a function to do sentiment classification
+def sentiment_classification(text: str) -> str:
+    """
+    This function takes a text input and returns the sentiment classification logits.
+    
+    Args:
+        text (str): The input text.
+        
+    Returns:
+        str: The sentiment classification class.
+    """
+    inputs = tokenizer(text, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+    logits = torch.softmax(logits, dim=1)
+    out=torch.argmax(logits)
+    idx2class = model.config.id2label
+    out=idx2class[out.item()]
+    return out
+# Test the function
+text = "I love you"
+out = sentiment_classification(text)
+print(out)
+# i made the code cramed up so that i test the pre-hook that i will make in next commits
