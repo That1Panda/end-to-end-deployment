9b2c9a44fedb69926c1c9031adb4f3540c8e32da
That1Panda <pandahd75@gmail.com>
2024-09-06 19:55:09 +0300

    [Development] Added pre-commit hook with black formatting and notebook output remover

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
 # Load the tokenizer and model
 # Create a function to do sentiment classification
 def sentiment_classification(text: str) -> str:
     """
     This function takes a text input and returns the sentiment classification logits.
-    
+
     Args:
         text (str): The input text.
-        
+
     Returns:
         str: The sentiment classification class.
     """
-    tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
-    model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
+    tokenizer = AutoTokenizer.from_pretrained(
+        "nlptown/bert-base-multilingual-uncased-sentiment"
+    )
+    model = AutoModelForSequenceClassification.from_pretrained(
+        "nlptown/bert-base-multilingual-uncased-sentiment"
+    )
     inputs = tokenizer(text, return_tensors="pt")
     outputs = model(**inputs)
     logits = outputs.logits
     logits = torch.softmax(logits, dim=1)
-    out=torch.argmax(logits)
+    out = torch.argmax(logits)
     idx2class = model.config.id2label
-    out=idx2class[out.item()]
+    out = idx2class[out.item()]
     return out
+
+
 # Test the function
 text = "I love you"
 out = sentiment_classification(text)
--- test_model.py
+++ test_model.py
@@ -1,11 +1,12 @@
 from main import sentiment_classification
 
+
 def test_sentiment_classification():
     text = "I love you"
     out = sentiment_classification(text)
-    assert out=="5 stars"
-    assert type(out)==str
-    
+    assert out == "5 stars"
+    assert type(out) == str
+
     text = "I hate you"
     out = sentiment_classification(text)
-    assert out=="1 star"
+    assert out == "1 star"
